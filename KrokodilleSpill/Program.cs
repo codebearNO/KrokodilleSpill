9729848e823ba8c5fc2aa754d86ddc70e718d6c2
Codebear <thorbjorn.hoib@hotmail.com>
2023-11-22 10:20:51 +0100

    Add project files.

---
+++ KrokodilleSpill/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks.Sources;
+
+namespace KrokodilleSpill
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int score = 0;
+            while (score < 10) 
+            {
+                int rand2 = random.Next(1, 11);
+                int rand1 = random.Next(1, 11);
+                Console.WriteLine($"Score: {score}");
+                Console.WriteLine($"{rand1} _ {rand2}");
+
+                var guess = Console.ReadLine();
+
+                int result = guess switch
+                {
+                    "=" => rand1 == rand2 ? score++ : score--,
+                    "<" => rand1 < rand2 ? score++ : score--,
+                    ">" => rand1 > rand2 ? score++ : score--,
+                    _ => score = 20
+                };
+                Console.Clear();
+            }
+
+            Console.WriteLine(score != 20 ? "You win!" : "You lose!");
+        }
+    }
+}
